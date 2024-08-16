97ac04a2c5d368a20731936a2f635c9be5a01449
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-08-16 20:13:56 +0700

    Update docker_install

--- docker_install.sh
+++ docker_install.sh
@@ -11,8 +11,14 @@ sudo service docker start
 ## Or
 # sudo systemctl start docker
 
+## Create docker group
+sudo groupadd docker
+
 ## Add user to docker
-sudo usermod -d -G docker ec2-user
+sudo usermod -aG docker $USER
+
+## Apply new change
+newgrp docker
 
 ## Test authorization
 docker ps
